ca7285e6efd986a7d4b5ba257a8e8a809106b377
memzzmem <110750952+memzzmem@users.noreply.github.com>
2025-07-25 18:18:53 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const WebSocket = require('ws');
 const server = new WebSocket.Server({port: process.env.PORT || 3000});
 server.on('connection', socket => {
     socket.on('message', message => {
-        socket.send(`Echo: ${message}`);
+        socket.send(message);
     });
 });
 console.log(`WebSocket server running on ws://localhost:${process.env.PORT || 3000}`);
